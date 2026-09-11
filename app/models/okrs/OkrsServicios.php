ff98588831f0fbcdc494365b5fd30a2993f387f8
Fabian VC <fabianvera728@gmail.com>
2026-09-11 11:35:02 -0500

    Leer VP y rol OKR de DatosEmpleado desde Punta Cana
    OkrsServicios::DatosEmpleado() tomaba la vicepresidencia de goforagile_admin
    y el rol OKR de goforagile_okrs, copias viejas de otra instalación. Solo lo
    llama reporte_lista_vicepresidencias(), que hoy no se usa desde ninguna vista.

--- app/models/okrs/OkrsServicios.php
+++ app/models/okrs/OkrsServicios.php
@@ -2599,8 +2599,8 @@ class OkrsServicios
             Cargos.nombre AS nombre_cargo,
             Areas.id AS id_area,
             Areas.nombre AS nombre_area,
-            goforagile_admin.Vicepresidencia.nombre AS nombre_vicepresidencia,
-            goforagile_okrs.Roles_Okrs.nombre_rol AS rol
+            puntacana_admin.Vicepresidencia.nombre AS nombre_vicepresidencia,
+            puntacana_okrs.Roles_Okrs.nombre_rol AS rol
         FROM
             Empleados
         LEFT JOIN
@@ -2608,11 +2608,11 @@ class OkrsServicios
         LEFT JOIN
             Areas ON Areas.id = Empleados.area
         LEFT JOIN
-            goforagile_admin.Estructura_Empresa ON Empleados.area = goforagile_admin.Estructura_Empresa.area
+            puntacana_admin.Estructura_Empresa ON Empleados.area = puntacana_admin.Estructura_Empresa.area
         LEFT JOIN
-            goforagile_admin.Vicepresidencia ON goforagile_admin.Vicepresidencia.id = goforagile_admin.Estructura_Empresa.vicepresidencia
+            puntacana_admin.Vicepresidencia ON puntacana_admin.Vicepresidencia.id = puntacana_admin.Estructura_Empresa.vicepresidencia
         LEFT JOIN
-            goforagile_okrs.Roles_Okrs ON goforagile_okrs.Roles_Okrs.id = Empleados.role
+            puntacana_okrs.Roles_Okrs ON puntacana_okrs.Roles_Okrs.id = Empleados.role
         WHERE
             Empleados.id = '$id'
         LIMIT 1
