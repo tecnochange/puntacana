2f41f12ba9cae276a4489d8489239dc60e42665e
Fabian VC <fabianvera728@gmail.com>
2026-09-11 11:15:35 -0500

    Leer los empleados de objetivo_asociado desde puntacana_admin
    OkrsServicios::objetivo_asociado() unía los integrantes del OKR con
    goforagile_admin, una copia vieja de otra instalación. Los datos del
    empleado deben salir de Punta Cana.

--- app/models/okrs/OkrsServicios.php
+++ app/models/okrs/OkrsServicios.php
@@ -831,8 +831,8 @@ class OkrsServicios
             Okrs_Equipos
         LEFT JOIN Okrs ON Okrs.id = Okrs_Equipos.id_okrs 
         LEFT JOIN Okrs_Areas ON Okrs_Areas.id_okrs = Okrs_Equipos.id_okrs
-        LEFT JOIN goforagile_admin.Empleados AS Empleados ON Empleados.id = Okrs_Equipos.id_empleado
-        LEFT JOIN goforagile_admin.Estructura_Empresa ON Okrs_Areas.id_area = goforagile_admin.Estructura_Empresa.area
+        LEFT JOIN puntacana_admin.Empleados AS Empleados ON Empleados.id = Okrs_Equipos.id_empleado
+        LEFT JOIN puntacana_admin.Estructura_Empresa ON Okrs_Areas.id_area = puntacana_admin.Estructura_Empresa.area
         LEFT JOIN Roles_Okrs ON Roles_Okrs.id_rol = Okrs_Equipos.tipo
         WHERE
             Okrs_Equipos.id_empresa = $id_empresa AND
